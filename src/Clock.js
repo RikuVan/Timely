2d563868ca7f0687c4b73fe0d0e7e3e6362afd9c
Richard Van Camp <richard.vancamp@gofore.com>
2017-10-01 13:41:48 +0300

    exercise 1

--- src/Clock.js
+++ src/Clock.js
@@ -2,20 +2,8 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Clock.css';
-import {formatTime} from './utils';
 
 class Clock extends Component {
-  state = {time: new Date()};
-
-  componentDidMount() {
-    this.task = window.setInterval(() => this.tick(), 20);
-  }
-
-  componentWillUnmount() {
-    window.clearInterval(this.task);
-  }
-
-  tick = () => this.setState(state => ({time: new Date()}));
 
   render() {
     return (
@@ -25,7 +13,7 @@ class Clock extends Component {
           <h1 className="App-title">Timely</h1>
         </header>
         <div className="Clock">
-          <p>{formatTime(this.state.time)}</p>
+          <p>00:00:00:00</p>
         </div>
       </div>
     );
